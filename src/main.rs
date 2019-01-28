fb94a901fa00cfef7e3484d23d28279d003a7cf0
Hasan Ali <hasanhaja@gmail.com>
2019-01-28 01:06:09 +0000

    Completed the ch on Tests.

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,60 @@ pub fn add_two(num: i32) -> i32 {
     num + 2
 }
 
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+    //String::from("Hello!")    // This would fail the test
+}
+
+struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+
+        // Splitting it up will
+        if value < 1 {
+            panic!("Guess value must be greater than or equal to 1, got {}.", value);
+        } else if value > 100 {
+            panic!("Guess value must be less than or equal to 100, got {}.", value);
+        }
+
+        Guess {
+            value
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     // Is there another way to do this?
     use super::*;
+
+    // NOTE: Since this is returning a Result, you cannot have the #[should_panic] with this function
+    #[test]
+    fn result_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+
+    #[test]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`", result
+        );
+    }
 
     #[test]
     fn it_adds_two() {
